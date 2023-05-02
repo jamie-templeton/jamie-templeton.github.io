4898f3b4f59c8ee5d029e25bd736fe0da3697721
jamie-templeton <98107385+jamie-templeton@users.noreply.github.com>
2023-05-02 17:49:25 -0400

    Add files via upload

---
+++ js/vim.js
@@ -0,0 +1,17 @@
+
+  options = {
+    id: 740858857,
+    texttrack: "en",
+    loop: true,
+    autoplay: true,
+    muted: true,
+    controls: false,
+    background: true,
+    responsive: true
+  };
+
+  player = new Vimeo.Player("player", options);
+
+
+
+
